7f9499880631f22d30cd29bfa39089869b62d798
Gregory <grishasobol@mail.ru>
2018-07-08 13:43:40 +0300

    add min functionality to class NetMapper
    add methods:  add_vertex, add_edge, get_vert_num, get_edge_num,
    get_path_to

--- include/net-mapper.hpp
+++ include/net-mapper.hpp
@@ -5,11 +5,36 @@
 #include "boost/graph/adjacency_list.hpp"
 #include "boost/graph/dijkstra_shortest_paths.hpp"
 
-class NetMapper{
+class NetMapper final{
+public:
+  using WeightType = double;
+  using Graph = boost::adjacency_list <
+    boost::vecS,
+    boost::vecS,
+    boost::undirectedS,
+    boost::no_property,
+    boost::property<boost::edge_weight_t, WeightType>>;
+  using vertex_descriptor = typename boost::graph_traits<Graph>::vertex_descriptor;
+  using edge_descriptor = typename boost::graph_traits<Graph>::edge_descriptor;
+private:
+  Graph graph;
+  void find_path(vertex_descriptor dest_vertex) const;
+  mutable std::map<vertex_descriptor, std::vector<vertex_descriptor>> path_preds_cache;
+  mutable std::set<vertex_descriptor> cached_vertices;
 public:
   NetMapper();
   NetMapper(const NetMapper& nm) = delete;
   NetMapper& operator=(const NetMapper& nm) = delete;
+  NetMapper(NetMapper&& nm) = delete;
+  NetMapper& operator=(NetMapper&& nm) = delete;
+
+  vertex_descriptor add_vertex();
+  edge_descriptor add_edge(vertex_descriptor first_vertex, 
+    vertex_descriptor second_vertex, WeightType weight);
+
+  size_t get_vert_num() const;
+  size_t get_edge_num() const;
+  std::vector<vertex_descriptor> get_path_to(vertex_descriptor dest_vertex) const;
 };
 
 #endif
--- net-mapper/net-mapper.cpp
+++ net-mapper/net-mapper.cpp
@@ -1,18 +1,51 @@
 #include "net-mapper.hpp"
 
 #include <iostream>
+#include <vector>
+#include <map>
+#include <set>
 
 using namespace boost;
 
-NetMapper::NetMapper() {
-  using Graph = adjacency_list<vecS, vecS, bidirectionalS>;
-  Graph g;
-  add_edge(1, 2, g);
-  add_edge(3, 2, g);
-  add_edge(1, 3, g);
-  add_edge(123, 124, g);
-  auto index = get(vertex_index, g);
-  for (auto vert = vertices(g); vert.first != vert.second; ++vert.first) {
-    std::cout << index[*vert.first] << " ";
+NetMapper::NetMapper() {}
+
+void NetMapper::find_path(vertex_descriptor dest_vertex) const {
+  using IdMap = typename boost::property_map<Graph, boost::vertex_index_t>::type;
+  using preds_map_t = boost::iterator_property_map<std::vector<vertex_descriptor>::iterator,
+    IdMap,
+    vertex_descriptor,
+    vertex_descriptor&>;
+
+  std::vector<vertex_descriptor> path_preds(num_vertices(graph));
+  preds_map_t predmap(path_preds.begin(), get(boost::vertex_index, graph));
+  dijkstra_shortest_paths(graph, dest_vertex, predecessor_map(predmap));
+
+  path_preds_cache.insert(std::make_pair(dest_vertex, std::move(path_preds)));
+  cached_vertices.insert(dest_vertex);
+}
+
+NetMapper::vertex_descriptor NetMapper::add_vertex() {
+  return boost::add_vertex(graph);
+}
+
+
+NetMapper::edge_descriptor NetMapper::add_edge(vertex_descriptor first_vertex, 
+  vertex_descriptor second_vertex, WeightType weight) {
+  return boost::add_edge(first_vertex, second_vertex, weight, graph).first;
+}
+
+size_t NetMapper::get_vert_num() const {
+  return num_vertices(graph);
+}
+
+size_t NetMapper::get_edge_num() const {
+  return num_edges(graph);
+}
+
+std::vector<NetMapper::vertex_descriptor> 
+NetMapper::get_path_to(NetMapper::vertex_descriptor dest_vertex) const {
+  if (cached_vertices.find(dest_vertex) == cached_vertices.end()) {
+    find_path(dest_vertex);
   }
+  return path_preds_cache[dest_vertex];
 }
