09068d1832ffa0fc1bb852a556b8d2ea1548393c
Gregory <grishasobol@mail.ru>
2018-07-08 14:33:10 +0300

    add tests for add_vertex and add_edge

--- unit-tests/TestMapper.cpp
+++ unit-tests/TestMapper.cpp
@@ -4,16 +4,41 @@
 class TestMapper : public ::testing::Test
 {
 protected:
-	void SetUp()
-	{
-	}
+  NetMapper mapper;
 };
 
-TEST_F(TestMapper, EmptyTest){
-	EXPECT_TRUE(true) << "What???";
-}
+class TestGraphMapper : public ::testing::Test
+{
+protected:
+  NetMapper mapper;
+  void SetUp() {
+
+  }
+};
 
 TEST_F(TestMapper, CreateMapper) {
-  NetMapper mapper;
   EXPECT_TRUE(true) << "What???";
+}
+
+TEST_F(TestMapper, CheckAddVertices) {
+  mapper.add_vertex();
+  mapper.add_vertex();
+  mapper.add_vertex();
+  mapper.add_vertex();
+  EXPECT_TRUE(mapper.get_vert_num() == 4) 
+    << "Excpect 4 verteces, but get: " 
+    << mapper.get_vert_num();
+}
+
+TEST_F(TestMapper, CheckAddEdges) {
+  auto v0 = mapper.add_vertex();
+  auto v1 = mapper.add_vertex();
+  auto v2 = mapper.add_vertex();
+  auto v3 = mapper.add_vertex();
+  auto e1 = mapper.add_edge(v0, v1, 10);
+  auto e2 = mapper.add_edge(v0, v2, 11);
+  auto e3 = mapper.add_edge(v0, v3, 12);
+  EXPECT_TRUE(mapper.get_edge_num() == 3) 
+    << "Excpect 3 edges, but get: "
+    << mapper.get_edge_num();
 }
